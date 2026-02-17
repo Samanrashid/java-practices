22182fa9ea45644b42563ab01a748ce3488347a0
Samaneasyeat <156658674+Samaneasyeat@users.noreply.github.com>
2026-02-17 23:07:03 +0530

    datatypes in variable and simple syntax declaration

---
+++ 01_basics/DataTypesDemo.java
@@ -0,0 +1,15 @@
+public class DataTypesDemo {
+  public static void main(String[] args) {
+    String name = "Saman";
+    int age = 26;
+    float percentage = 85.4f;
+    float height = 5.3f;
+    char favourite = 's';
+    System.out.println("name is:" + name);
+    System.out.println("Age is : " + age);
+    System.out.println("percentage is :" + percentage);
+    System.out.println("height is : " + height);
+    System.out.println("favourite is : " + favourite);
+  }
+  
+}
