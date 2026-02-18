d920b34f7e15e7c91d8d48d2446d891b172391dd
Samaneasyeat <156658674+Samaneasyeat@users.noreply.github.com>
2026-02-18 11:47:37 +0530

    simple program to take input in java

---
+++ 01_basics/UserInputDemo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class UserInputDemo {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("enter name: ");
+    String name = scanner.nextLine();
+    System.out.println("name is : " + name);
+
+
+
+    
+  }
+  
+}
